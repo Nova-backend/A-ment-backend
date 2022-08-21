062c09cc3c6c601ecf4d67aa02e0e03f32c2a0eb
Divine Ingabire <94451652+Divine-20@users.noreply.github.com>
2022-08-21 19:05:14 +0200

    Revert "Revert "Divine""

---
+++ auth/user.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+
+module.exports.generateAUthToken = (userId)=>{
+  const token = jwt.sign({userId}, process.env.SECRET_KEY, {expiresIn:'24h'})
+  return token;
+}
+module.exports.verifyToken = (req,res,next) =>{
+    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
+        const token = jwt.verify(req.headers.authorization.split(" ")[1], process.env.SECRET_KEY)
+    if(!token) {
+        return res.status(401).send("you have to log in");
+    }else{
+        return req.token  = token
+    }
+    }
+}
