68b74ecc3a4bab058c0f933f6ae546b325e27ae3
Divine-20 <divineingabire69@gmail.com>
2022-08-21 18:10:24 +0200

    working on the router

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const { signup } = require('../contollers/useInfo')
-const { signup} = require("../contollers/useInfo")
+const { verifyToken } = require('../auth/user')
 const router = require('express').Router()
 
-module.exports.Router = (app)=>{
+module.exports.Router=(app)=>{
     router.post('/signup', signup())
 }
