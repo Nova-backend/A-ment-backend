e322de7344203e3acd4394182b0089474bf43c32
Divine Ingabire <94451652+Divine-20@users.noreply.github.com>
2022-08-22 17:41:08 +0200

    Revert "Revert "Revert "Revert "Divine""""

---
+++ models/userModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const joi = require('joi')
+const User = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+
+})
+
+const validation = (data)=>{
+     data = new joi.object({
+        firstName: joi.string().required().min(4),
+        lastName: joi.string().required().min(4),
+        email: joi.string().required().email(),
+        password: joi.string().required().email().min(8)
+    })
+    return data.validate()
+}
+
+const OTP = mongoose.Schema({
+    OTP : {
+        type: String,
+        required: true
+    }, 
+    email:{
+        type: String,
+        required: true
+    }
+})
+module.exports.validation = validation
+module.exports.User = mongoose.model('user', User)
+module.exports.OTPmodel = mongoose.model('OTPs', OTP)
