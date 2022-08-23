e8aa1308cd00355c6440cfd2257216b2fc2e5919
Divine Ingabire <94451652+Divine-20@users.noreply.github.com>
2022-08-23 11:17:32 +0200

    Revert ""Divine""""""""

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.URL).then(()=>{
+    console.log("Database successfully connected");
+})
+
+
+const PORT = process.env.PORT
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.listen(PORT, ()=>{
+    console.log(`The server is learning on port ${PORT}`)
+})
+
