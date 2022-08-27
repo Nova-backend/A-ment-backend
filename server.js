9383209d5f439c0da910a23e14bf6fb0b6256386
Divine-20 <divineingabire69@gmail.com>
2022-08-27 12:13:02 +0200

    required cloudinary and formidable

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
+const formidable = require('formidable')
+const cloudinary = require('cloudinary')
 // const {Router} = require('./routes/user.js')
 const express = require('express')
 const app = express()
