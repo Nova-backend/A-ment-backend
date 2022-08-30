d54925d409abb06f95202a23bb1e716647025a22
Divine-20 <divineingabire69@gmail.com>
2022-08-30 11:17:33 +0200

    added the cloudinary options to signup

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
-const formidable = require('formidable')
-const cloudinary = require('cloudinary')
+// const formidable = require('formidable')
+// const cloudinary = require('cloudinary')
 // const {Router} = require('./routes/user.js')
 const express = require('express')
 const app = express()
@@ -12,17 +12,8 @@ const router = require('./routes/user.js')
 mongoose.connect(process.env.URL).then(()=>{
     console.log("Database successfully connected");
 })
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-    secure:true
-});
 
-cloudinary.uploader.upload("https://res.cloudinary.com/dzgesd2uy/image/upload/v1657472998/cld-sample-2.jpg",
- function(error, result) {
-     console.log(result, error)
-    });
+
 
 
 const PORT = process.env.PORT
