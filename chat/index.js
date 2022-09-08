8cc1502490be0f3f8fe0d747a9bac850a4028df1
Divine-20 <divineingabire69@gmail.com>
2022-09-08 10:22:46 +0200

    some changes made

--- chat/index.js
+++ chat/index.js
@@ -1,5 +1,6 @@
-const io = require("socket.io")(3000);
+const io = require('socket.io')(3000);
 
 io.on('connection', socket => {
+   console.log("new user");
    socket.emit("chat-message", "Welcome! Chat with us"); 
 })
