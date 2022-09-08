196fc463c3c4e3e7e6c6fbe3e381892703f56afc
Divine-20 <divineingabire69@gmail.com>
2022-09-08 10:56:27 +0200

    installed some packages

--- chat/index.js
+++ chat/index.js
@@ -1,6 +1,7 @@
-const io = require('socket.io')(3000);
 
+ const io = require("socket.io").listen(3000)
 io.on('connection', socket => {
    console.log("new user");
    socket.emit("chat-message", "Welcome! Chat with us"); 
 })
+module.exports = io
