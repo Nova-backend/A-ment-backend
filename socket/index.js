e4e623f9b3fe21ae6f75ad4f61e726c22649a7ca
Divine-20 <divineingabire69@gmail.com>
2022-09-10 12:01:13 +0200

    required the scripts

--- socket/index.js
+++ socket/index.js
@@ -2,5 +2,7 @@ const io = require('socket.io')(3000);
 
 
 io.on('connection', socket=>{
+    console.log("new user")
     socket.emit('chat-message', "Welcome! Chat with us")
+   
 })
--- socket/script.js
+++ socket/script.js
@@ -1,3 +1,8 @@
 const socket = io("http://localhost:3000")
 
+socket.on("chat-message", data=>{
+  console.log(data);
+
+})
+
 
