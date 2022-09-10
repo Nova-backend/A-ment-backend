d9768307a6da3925594d0b73d9e060ed6cc9a0f6
Divine-20 <divineingabire69@gmail.com>
2022-09-10 12:16:34 +0200

    installed a package

--- socket/script.js
+++ socket/script.js
@@ -1,4 +1,9 @@
-const socket = io("http://localhost:3000")
+const express = require('express')
+
+
+const app = express();
+const server = http.createServer(app);
+const socket = io("http://localhost:3000")(server)
 
 socket.on("chat-message", data=>{
   console.log(data);
