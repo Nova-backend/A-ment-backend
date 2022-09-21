b7fc55f7c39947adaa3832477aea8db06eeae43e
Divine-20 <divineingabire69@gmail.com>
2022-09-21 11:02:44 +0200

    removed duplicate

--- routes/user.js
+++ routes/user.js
@@ -5,17 +5,14 @@ const { User } = require('../models/userModel.js')
 const express = require('express')
 
 const router = express.Router()
-router.post('/signup',signup())
+
 router.put('/signup/:id', updateUser())
 router.delete('/signup/:id', deleteUser())
 router.get('/signup', getUser())
-<<<<<<< HEAD
-=======
 router.post('/signup', signup())  
 
 
 
   
 
->>>>>>> b1eb0b003a5a20f607857e6f4d4a2bb609d7f9f6
 module.exports = router;
