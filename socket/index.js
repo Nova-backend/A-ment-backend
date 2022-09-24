6719cd65affbdbac835a0c850deac3eea1d330b9
Divine-20 <divineingabire69@gmail.com>
2022-09-24 13:36:07 +0200

    some changes made

--- socket/index.js
+++ socket/index.js
@@ -8,10 +8,6 @@ const {
 } = require('socket.io');
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-});
-
 io.on('connection', (socket) => {
     console.log('a user connected');
 });
