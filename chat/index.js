35f0fc1a1ad5e7d8eec0c66b9ea64dae7c7edb81
Divine-20 <divineingabire69@gmail.com>
2022-09-25 18:12:04 +0200

    working on chat

--- chat/index.js
+++ chat/index.js
@@ -4,11 +4,36 @@ const io  = require("socket.io")(8000, {
     }
 })
 
+const users = {}
+
+
+const addUser = (userId, socketId) => {
+   !users.some((user) => user.userId === userId && 
+   users.push({userId,socketId})
+   )
+   console.log(users);
+   
+}
+
+const removeUser = (userId) => {
+    users = users.filter((user) => user.socketId !== socketId)
+}
+
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId
+    )
+}
 io.on("connection", (socket) => {
-    socket.on("send-chat-message", message=>{
-        socket.broadcast.emit("chat-message", message)
-    })
+    // socket.on("send-chat-message", message=>{
+    //     socket.broadcast.emit("chat-message", {message: message, name:users[socket.id]})
+    // })
+    // socket.on("new-user", name => {
+    //     users[socket.id] = name;
+    //     socket.broadcast.emit("user connected", name)
+    // })
+    console.log(socket.id, "User connected");
 })
-io.on("disconnection", (socket) => {
-    console.log("Disconnected");
+io.on("disconnection", () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
 })
