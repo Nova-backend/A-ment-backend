6c8a2b86b6f0ad8542d7696889d42ced7274712a
Divine-20 <divineingabire69@gmail.com>
2022-10-03 21:34:43 +0200

    created an appointment model

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -1,1 +1,21 @@
-const mongoose = require('mongoose')
+// const { StreamingQuerystring } = require('formidable/parsers');
+const mongoose = require('mongoose');
+const Appointment = mongoose.Schema({
+    service:{
+        type:String,
+        required:true,
+    },
+    clientName:{
+        type:String,
+        required:true,
+    },
+    Date:{
+        type:Date,
+        required:true
+    },
+    Duration:{
+        typpe:Date,
+        required:true
+    }
+})
+module.exports.appointment = mongoose.model('appointment',Appointment);
