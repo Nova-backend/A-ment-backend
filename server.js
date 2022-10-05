d3abbd99a5ac467fb5cf2d679f94c7c2a6793481
Divine-20 <divineingabire69@gmail.com>
2022-10-05 21:20:37 +0200

    uninstalled some packages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 
 dotenv.config()
-const keys = require('./')
+
 const path = require('path')
 const fileupload = require("express-fileupload")
 const express = require('express')
@@ -14,24 +14,11 @@ const cookieSession = require('cookie-session')
 mongoose.connect(process.env.URL).then(()=>{
     console.log("Database successfully connected");
 })
-
-
-
-
 const PORT = process.env.PORT
 app.use(fileupload({useTempFiles:true}))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use("/",router)
-
-app.use(cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey],
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.listen(PORT, ()=>{
     console.log(`The server is learning on port ${PORT}`)
 })
