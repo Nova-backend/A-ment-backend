51c0dbe3852c5a731a713f2854617a57df36590f
Divine-20 <divineingabire69@gmail.com>
2022-10-05 17:20:05 +0200

    installed a new package

--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,7 @@ const {
 const {createAppointment} = require('../contollers/appointmentContoller')
 const { verifyToken } = require('../auth/user')
 const { User } = require('../models/userModel.js')
+const passport = require('passport')
 
 const express = require('express')
 
@@ -21,8 +22,18 @@ router.post('/login',login())
 router.put('/forgotpassword', forgotPassword())
 router.post('/createAppointment', createAppointment())
 
-
-
-  
+module.exports = (app)=>{
+    app.get('/auth/google', passport.authenticate('google',{
+        scope:['profile','email']
+    }))
+    app.get('/auth/google/callback',passport.authenticate('google'));
+    app.get('api/logout',(req,res)=>{
+        req.logout();
+        res.send(req.user);
+    });
+    app.get('/api/current_user',(req,res)=>{
+        res.send(req.user);
+    })
+}
 
 module.exports = router;
