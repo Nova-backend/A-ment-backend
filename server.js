78bf301f4a699db0b29306d1c4654126f04d6b94
Divine-20 <divineingabire69@gmail.com>
2022-10-12 20:36:44 +0200

    removed bug

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ dotenv.config()
 const fileupload = require("express-fileupload")
 
 const express = require('express')
+
+const bodyparser= require('body-parser');
 const app = express()
 // const server = http.createServer(app)
 const mongoose = require('mongoose')
@@ -14,7 +16,8 @@ const router = require('./routes/user.js')
 const manageappoint=require('./routes/requestAppointRoutes')
 
 new Swaggiffy().setupExpress(app).swaggiffy();
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const { urlencoded } = require('express');
 
 
 
@@ -24,6 +27,7 @@ mongoose.connect(process.env.URL).then(()=>{
 const PORT = process.env.PORT
 app.use(fileupload({useTempFiles:true}))
 app.use(express.json())
+app.use(bodyparser)
 app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
 app.use("/",router)
