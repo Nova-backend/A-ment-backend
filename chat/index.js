e2ef1d7f927d2c4883a281b551cf2d788abc3c15
Divine-20 <divineingabire69@gmail.com>
2022-10-17 16:03:13 +0200

    connected the chat-app to the database

--- chat/index.js
+++ chat/index.js
@@ -8,12 +8,17 @@ const server = app.listen(port,
     )
 // app.use(express.static(path.join(__dirname + '/public')))
 app.use("/static", express.static('./static/'));
-
+const mongoose = require('mongoose')
 const io  = require("socket.io")(server, {
     cors : {
-        origin : "*"
+        origin : "*",
+        method:["POST","GET"]
     }
 })
+mongoose.connect("mongodb+srv://Divine:123@cluster0.0e37a.mongodb.net/Appointments_data?retryWrites=true&w=majority").then(()=>{
+    console.log("Database successfully connected");
+})
+
 const users = {}
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
@@ -34,7 +39,7 @@ const getUser = (userId) => {
 }
 //connecting the user
 io.on("connection", socket => {
-    console.log(socket.id, "User connected");
+    console.log("User connected with an ID of: " ,socket.id);
     //user connected
      socket.on('new-user', name => {
                 users[socket.id] = name;
@@ -48,7 +53,7 @@ io.on("connection", socket => {
 })   
 socket.on('chat', message => {
     console.log('From server: ', message)
-    // io.emit('chat', message)
+    
   })
 //adding user
     socket.on("addUser", (data)=>{
