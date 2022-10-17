97aa53257fe82179a5c83c3df368cac85cda9628
Divine-20 <divineingabire69@gmail.com>
2022-10-17 17:18:30 +0200

    removed bug

--- chat-app/chat.js
+++ chat-app/chat.js
@@ -1,65 +1,72 @@
+
+module.exports.chatting = () =>{
+    return async(req,res) =>{
+
+    
 let ejs = require('ejs');
 const express = require('express')
 const app = express()
 const path = require('path')
-
-const server = app.listen(Process.env.URL)
+const dotenv = require('dotenv')
+dotenv.config()
+const server = app.listen(process.env.PORT,)
 app.use("/static", express.static('./static/'));
-const mongoose = require('mongoose')
+
 const io  = require("socket.io")(server, {
     cors : {
         origin : "*",
-        method:["POST","GET"]
+        // method:["POST","GET"]
     }
 })
 
-
-const users = {}
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
     res.render('chat');
   });
+const Chat = require('../models/chatModel')
+const users = {};
+
+  app.get('/', (req, res) => {
+    Chat.find({}).then(messages => {
+      res.render('index', {messages});
+    }).catch(err => console.error(err));
+  });
+
+  io.sockets.on('connection', socket => {
+  socket.on('chat', data => {
+    Chat.create({name: data.handle, message: data.message}).then(() => {
+      io.sockets.emit('chat', data); // return data
+    }).catch(err => console.error(err));
+  });
+  socket.on('typing', data => {
+    socket.broadcast.emit('typing', data); // return data
+});
+socket.on("addUser", (data)=>{
+    addUser(data,socket.id);
+    socket.broadcast.emit("getUsers",users)
+    getUser(socket.id);
+})
+});
+  
 const addUser = (userId, socketId) => {
    !users.some((user) => user.userId === userId && 
    users.push({userId,socketId})
    )
    console.log(users);
 }
-const removeUser = (userId) => {
+const removeUser = (socketId) => {
     users = users.filter((user) => user.socketId !== socketId)
 }
 const getUser = (userId) => {
     return users.find((user) => user.userId === userId
     )
 }
-//connecting the user
-io.on("connection", socket => {
-    console.log("User connected with an ID of: " ,socket.id);
-    //user connected
-     socket.on('new-user', name => {
-                users[socket.id] = name;
-                socket.broadcast.emit("user connected", name)
-            })
-    //sending message
-    socket.on('chat', message => {
-    console.log('From client: ', message);
-    io.emit('chat', message)
 
-})   
-socket.on('chat', message => {
-    console.log('From server: ', message)
-    
-  })
-//adding user
-    socket.on("addUser", (data)=>{
-        addUser(data,socket.id);
-        socket.broadcast.emit("getUsers",users)
-        getUser(socket.id);
-    })
- 
-})
 io.on("disconnection", () => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
     removeUser(socket.id)
 })
+    }
+}
+
