838536acde997648a5c8fafac2a6875da4390df3
Divine-20 <divineingabire69@gmail.com>
2022-10-22 15:17:19 +0200

    ensured validation

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -1,6 +1,7 @@
 // const { StreamingQuerystring } = require('formidable/parsers');
 const mongoose = require("mongoose");
 const { registerSchema } = require("swaggiffy");
+const joi = require("joi");
 const Appointment = mongoose.Schema({
   service: {
     type: String,
@@ -19,5 +20,17 @@ const Appointment = mongoose.Schema({
     required: true,
   },
 });
+const validation = (data) => {
+ data = new joi.object({
+  service:joi.string().required(),
+  clientName:joi.string().required(),
+  date:joi.date().required(),
+  duration:joi.date()
+ })
+
+}
+
+module.exports.validation = validation;
 module.exports.Appointment = mongoose.model("appointment", Appointment);
 registerSchema("appointment", Appointment);
+registerSchema("validation", validation);
