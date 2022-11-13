11165921c0f52562b139ff096f25224aff0814ba
Divine-20 <divineingabire69@gmail.com>
2022-11-13 16:38:01 +0200

    changed datatypes

--- models/momoPaymentModel.js
+++ models/momoPaymentModel.js
@@ -1,19 +1,43 @@
+const { string } = require('joi');
 const mongoose = require('mongoose');
 
 const PaymentDetails =  mongoose.Schema({
     phone_number: {
-        type:Number,
+        type:String,
         required:true
     },
     amount: {
         
-            type:Number,
+            type:String,
             required:true
         
     },
-    email:{
-        type:String,
-        required:true
-    }
+   email: {
+    type:String,
+    required:true
+   },
+   fullname: {
+    type:String,
+    required:true,
+   },
+   currency: {
+    type:String,
+    required:true
+   
+   },
+
+   payment_type: {
+    type:String,
+    required:true
+   },
+   order_id :{
+   type:String,
+
+   },
+   tx_ref: {
+    type:String,
+  
+   }
+
 })
 module.exports.PaymentDetails = mongoose.model("payload", PaymentDetails)
