ba5ca92aee94abd13af8fc65545740a32ec2af2a
Divine-20 <divineingabire69@gmail.com>
2023-01-05 16:58:09 +0200

    added some content

--- routes/client.js
+++ routes/client.js
@@ -6,4 +6,6 @@ const { signUp, signin } = require("../contollers/clientUser");
 clientRouter.post("/signup", signUp);
 clientRouter.post("/signin", signin);7
 registerDefinition(clientRouter, {tags:'Client', mappedSchema:'Client',basePath:'/client'})
+registerDefinition(clientRouter, {tags:'Client', mappedSchema:'Clientgoogle',basePath:'/client'})
 module.exports.clientRouter = clientRouter;
+Client
